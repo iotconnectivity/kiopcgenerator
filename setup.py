7683675a14f07864d122b669ba4430631fc6ecfa
J. Félix Ontañón <felix.ontanon@podgroup.com>
2020-03-12 17:14:57 +0100

    v0.1.4 Added requirements to ease the installation of dependencies

--- setup.py
+++ setup.py
@@ -16,22 +16,15 @@ setup(
     description="OPc USIM card keys geneartion. This script will produce Ki/eKi/OPc triplets given the Op and Transport keys.",
     long_description=open("README.md", "r").read(),
     classifiers=[
-        'Development Status :: 0.1.2 - Beta',
+        'Development Status :: 0.1.4 - Beta',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Topic :: Utilities'
     ],
     keywords = ['python', 'sim', 'simcard', 'telecoms'],
-    version="0.1.2",
+    version="0.1.4",
     license="GPLv3",
-    setup_requies=['wheel'],
-    install_requires=[
-        'pycrypto',
-        # I cannot undertstand how such a specific dependency like the following
-        # installing card-0.0.1 instead of card-0.3.1. I'm afraid you'll have to manually install this 
-        'card @ git+https://github.com/PodgroupConnectivity/card.git@b8eb2c70eaad754a631a9fcbd1ce0dba4b58a662#egg=card-0.3.1'
-    ],
     packages=['kiopcgenerator'],
     scripts = ["kiopcgen"]
 )
